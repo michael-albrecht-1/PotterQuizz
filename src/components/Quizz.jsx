2402513d067fc20900633fe1fad4ba0abe220c69
Albrecht <albrecht.michael@hotmail.fr>
2020-09-18 16:56:24 +0200

    roll question

--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -7,11 +7,13 @@ function Quizz(props) {
     return <div className="card-body">Loading !</div>;
 
   const rollId = Math.floor(Math.random() * apiData.length);
+  const question = generateQuestion(apiData, rollId);
 
   return (
-    <div className="card-body">
-      <div className="mb-5">
-        What is the <strong>{apiData[rollId].name}</strong> Poudlard House ?
+    <div>
+      <div className="card-body">
+        {question}
+        <div>Answer : {apiData[rollId].house}</div>
       </div>
       <div className="alert alert-primary"> {JSON.stringify(apiData)}</div>
     </div>
@@ -19,3 +21,33 @@ function Quizz(props) {
 }
 
 export default Quizz;
+
+function generateQuestion(apiData, rollId) {
+  const questionType = rollQuestionType(apiData);
+  if (apiData[rollId].questionType === null) {
+  }
+
+  return (
+    <div className="mb-5">
+      What is the <strong>{apiData[rollId].name}</strong> Poudlard House ?
+    </div>
+  );
+}
+
+function rollQuestionType(apiData, rollId) {
+  let selectedQuestionType = null;
+  const roll = Math.floor(Math.random() * 7);
+  switch (roll) {
+    case value:
+      1;
+      selectedQuestionType = "role";
+      break;
+
+    default:
+      break;
+  }
+
+  while (apiData[rollId] !== null) {}
+
+  return "house";
+}
