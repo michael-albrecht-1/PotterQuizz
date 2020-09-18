57b4091c4e924f77a690d194f646ad93dd342ae3
Albrecht <albrecht.michael@hotmail.fr>
2020-09-18 12:58:19 +0200

    question

--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -3,12 +3,19 @@ import React from "react";
 function Quizz(props) {
   const { apiData } = props;
 
-  if (apiData.length === 0)
-    return <div className="card-body">Something went wrong !</div>;
+  if (!apiData || apiData.length === 0)
+    return <div className="card-body">Loading !</div>;
 
   const rollId = Math.floor(Math.random() * apiData.length);
 
-  return <div className="card-body">{rollId}</div>;
+  return (
+    <div className="card-body">
+      <div className="mb-5">
+        What is the <strong>{apiData[rollId].name}</strong> Poudlard House ?
+      </div>
+      <div className="alert alert-primary"> {JSON.stringify(apiData)}</div>
+    </div>
+  );
 }
 
 export default Quizz;
