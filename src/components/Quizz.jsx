6d5039dab8ec01890e92396f8caeece99cf0d714
Albrecht <albrecht.michael@hotmail.fr>
2020-09-18 11:53:23 +0200

    rollName

--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 function Quizz(props) {
   const { apiData } = props;
-  //if (apiData.length !== 0) {
-  return (
-    <div className="alert alert-success">
-      apiData : {JSON.stringify(apiData)}
-    </div>
-  );
+
+  if (apiData.length === 0)
+    return <div className="card-body">Something wen wrong</div>;
+
+  const rollId = Math.floor(Math.random() * apiData.length);
+
+  return <div className="card-body">{rollId.name}</div>;
   //}
 }
 
