fe2d8eb53863336c84d80bd5a5f7e9a62ae27ddf
Albrecht <albrecht.michael@hotmail.fr>
2020-09-18 19:49:07 +0200

    switchCase question/answers

--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -6,14 +6,13 @@ function Quizz(props) {
   if (!apiData || apiData.length === 0)
     return <div className="card-body">Loading !</div>;
 
-  const rollId = Math.floor(Math.random() * apiData.length);
-  const question = generateQuestion(apiData, rollId);
+  const question = generateQuestion(apiData);
 
   return (
     <div>
       <div className="card-body">
-        {question}
-        <div>Answer : {apiData[rollId].house}</div>
+        <div className="card-text">{question.question}</div>
+        <div className="card-text">{question.answer}</div>
       </div>
       <div className="alert alert-primary"> {JSON.stringify(apiData)}</div>
     </div>
@@ -22,32 +21,47 @@ function Quizz(props) {
 
 export default Quizz;
 
-function generateQuestion(apiData, rollId) {
-  const questionType = rollQuestionType(apiData);
-  if (apiData[rollId].questionType === null) {
+function generateQuestion(apiData) {
+  let id = null;
+  let rollId = null;
+  let selectedQuestion = {};
+  const roll = Math.floor(Math.random() * Math.floor(3));
+  let continueWhile = true;
+
+  while (continueWhile) {
+    rollId = Math.floor(Math.random() * apiData.length);
+    id = apiData[rollId];
+    switch (roll) {
+      case 0:
+        if (!id.role || id.role === null) {
+          break;
+        }
+        selectedQuestion.question = "What is " + id.name + " role ?";
+        selectedQuestion.answer = "Answer : " + id.role;
+        continueWhile = false;
+        break;
+
+      case 1:
+        if (!id.patronus || id.patronus === null) {
+          break;
+        }
+        selectedQuestion.question = "What is " + id.name + " patronus ?";
+        selectedQuestion.answer = "Answer : " + id.patronus;
+        continueWhile = false;
+        break;
+
+      case 2:
+        if (!id.house || id.house === null) {
+          break;
+        }
+        selectedQuestion.question = "What is " + id.name + " house ?";
+        selectedQuestion.answer = "Answer : " + id.house;
+        continueWhile = false;
+        break;
+
+      default:
+        break;
+    }
   }
-
-  return (
-    <div className="mb-5">
-      What is the <strong>{apiData[rollId].name}</strong> Poudlard House ?
-    </div>
-  );
-}
-
-function rollQuestionType(apiData, rollId) {
-  let selectedQuestionType = null;
-  const roll = Math.floor(Math.random() * 7);
-  switch (roll) {
-    case value:
-      1;
-      selectedQuestionType = "role";
-      break;
-
-    default:
-      break;
-  }
-
-  while (apiData[rollId] !== null) {}
-
-  return "house";
+  return selectedQuestion;
 }
